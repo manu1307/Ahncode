79839b8f8e8a07771096deec4e57da04a706e9a1
Sung Chul Park <jasonsc@naver.com>
2024-11-27 15:30:51 +0900

    앱 제목 및 설명 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,19 +5,16 @@ import { RecoilRoot } from "recoil";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<RecoilRoot>
-			<Head>
-				<title>LeetClone</title>
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<link rel='icon' href='/favicon.png' />
-				<meta
-					name='description'
-					content='Web application that contains leetcode problems and video solutions'
-				/>
-			</Head>
-			<ToastContainer />
-			<Component {...pageProps} />
-		</RecoilRoot>
-	);
+  return (
+    <RecoilRoot>
+      <Head>
+        <title>AhnCode</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+        <meta name="description" content="Lets solve some code problems!" />
+      </Head>
+      <ToastContainer />
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
