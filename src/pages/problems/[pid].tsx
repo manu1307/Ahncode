109e3aec13e9a1e164749396034352f0e4280e14
Sung Chul Park <jasonsc@naver.com>
2024-11-28 16:07:56 +0900

    문제 페이지에서 Firebase를 사용하여 문제 데이터를 가져오도록 수정

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,57 +1,96 @@
 import Topbar from "@/components/Topbar/Topbar";
 import Workspace from "@/components/Workspace/Workspace";
+import { firestore } from "@/firebase/firebase";
 import useHasMounted from "@/hooks/useHasMounted";
-import { problems } from "@/utils/problems";
+// import { problems } from "@/utils/problems";
 import { Problem } from "@/utils/types/problem";
+import { doc, getDoc } from "firebase/firestore";
 import React from "react";
 
 type ProblemPageProps = {
-	problem: Problem;
+  problem: Problem;
 };
 
 const ProblemPage: React.FC<ProblemPageProps> = ({ problem }) => {
-	const hasMounted = useHasMounted();
+  const hasMounted = useHasMounted();
 
-	if (!hasMounted) return null;
+  if (!hasMounted) return null;
 
-	return (
-		<div>
-			<Topbar problemPage />
-			<Workspace problem={problem} />
-		</div>
-	);
+  return (
+    <div>
+      <Topbar problemPage />
+      <Workspace problem={problem} />
+    </div>
+  );
 };
 export default ProblemPage;
 
 // fetch the local data
 //  SSG
-// getStaticPaths => it create the dynamic routes
 export async function getStaticPaths() {
-	const paths = Object.keys(problems).map((key) => ({
-		params: { pid: key },
-	}));
+  // const paths = Object.keys(problems).map((key) => ({
+  //   params: { pid: key },
+  // }));
 
-	return {
-		paths,
-		fallback: false,
-	};
+  // 가능한 문제 경로들
+  const paths = [
+    {
+      params: {
+        pid: "two-sum",
+      },
+    },
+    {
+      params: {
+        pid: "reverse-linked-list",
+      },
+    },
+    {
+      params: {
+        pid: "jump-game",
+      },
+    },
+    {
+      params: {
+        pid: "search-a-2d-matrix",
+      },
+    },
+    {
+      params: {
+        pid: "valid-parentheses",
+      },
+    },
+  ];
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 // getStaticProps => it fetch the data
 
 export async function getStaticProps({ params }: { params: { pid: string } }) {
-	const { pid } = params;
-	const problem = problems[pid];
+  const { pid } = params;
+  // const problem = problems[pid];
+  const docRef = doc(firestore, "problems_set", pid);
+  const docSnap = await getDoc(docRef);
+  const problem = docSnap.data();
 
-	if (!problem) {
-		return {
-			notFound: true,
-		};
-	}
-	problem.handlerFunction = problem.handlerFunction.toString();
-	return {
-		props: {
-			problem,
-		},
-	};
+  // if (docSnap.exists()) {
+  //   console.log("Document data:", docSnap.data());
+  // } else {
+  //   console.log("No such document!");
+  // }
+
+  if (!problem) {
+    return {
+      notFound: true,
+    };
+  }
+  problem.handlerFunction = problem.handlerFunction.toString();
+  return {
+    props: {
+      problem,
+    },
+  };
 }
--- src/utils/problems/index.ts
+++ src/utils/problems/index.ts
@@ -2,17 +2,17 @@ import { Problem } from "../types/problem";
 import { jumpGame } from "./jump-game";
 import { reverseLinkedList } from "./reverse-linked-list";
 import { search2DMatrix } from "./search-a-2d-matrix";
-import { twoSum } from "./two-sum";
+// import { twoSum } from "./two-sum";
 import { validParentheses } from "./valid-parentheses";
 
 interface ProblemMap {
-	[key: string]: Problem;
+  [key: string]: Problem;
 }
 
-export const problems: ProblemMap = {
-	"two-sum": twoSum,
-	"reverse-linked-list": reverseLinkedList,
-	"jump-game": jumpGame,
-	"search-a-2d-matrix": search2DMatrix,
-	"valid-parentheses": validParentheses,
-};
+// export const problems: ProblemMap = {
+// 	"two-sum": twoSum,
+// 	"reverse-linked-list": reverseLinkedList,
+// 	"jump-game": jumpGame,
+// 	"search-a-2d-matrix": search2DMatrix,
+// 	"valid-parentheses": validParentheses,
+// };
